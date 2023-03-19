9807579a8879e73e59f194eccf0e306c0f63f45f
Beeyour <65689511+Beeyour@users.noreply.github.com>
2023-03-19 14:11:32 +0300

    Add files via upload

---
+++ commands/stuff/ping.js
@@ -0,0 +1,18 @@
+const {
+  SlashCommandBuilder,
+  ChatInputCommandInteraction,
+  Client,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
+  /**
+   * @param {ChatInputCommandInteraction} interaction
+   * @param {Client} client
+   */
+  async execute(interaction, client) {
+    await interaction.reply("Pong!");
+  },
+};
---
+++ index.js
@@ -0,0 +1,13 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const { TOKEN } = require("./JSON/config.json");
+const { readdirSync } = require("node:fs");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds],
+});
+
+readdirSync("./handlers").forEach((handler) => {
+  require(`./handlers/${handler}`)(client);
+});
+
+client.login(TOKEN);
